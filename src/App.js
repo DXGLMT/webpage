df9c60b621026dcad4ecc3374d3b636e810f2c83
DXGLMT <zion.odimegwu@pau.edu.ng>
2023-06-01 16:27:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import InventoryList from './components/InventoryList';
+import Form from './components/Form';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [artWorks, setArtWorks] = useState([]);
+
+  // Function to add a new art work to the inventory
+  const addArtWork = (newArtWork) => {
+    setArtWorks([...artWorks, newArtWork]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Museum Inventory Management</h1>
+      <Form addArtWork={addArtWork} />
+      <InventoryList artWorks={artWorks} />
     </div>
   );
-}
+};
 
 export default App;
